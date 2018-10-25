e650356b28aafa95bd4799108ed137eaf719be17
RUInsYi <yifortean@outlook.com>
2018-10-25 19:39:56 +0800

    Site updated: 2018-10-25 19:39:52

---
+++ js/myTest.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+  
+  // sidebar slide
+  $('.sidebar-toggle').click(function(e){
+    $('.menu-sidebar').toggleClass('in');
+  });
+  
+  /*
+  $('.main-content').click(function(e){
+    $('.menu-sidebar').removeClass('in');
+  });
+  */
+}
+)
